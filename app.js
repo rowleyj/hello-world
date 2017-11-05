0abf31b466bbb64e4f396c4642a31538c7b37ea2
Jesse Rowley <rowleyj@mcmaster.ca>
2017-11-04 22:39:37 -0400

    setting up for adam

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var path = require('path');
-var expressValidator = require('express-validator'); //form validation
 var mongojs = require('mongojs'); //database scripting
-
+var path = require('path');
 
 var app = express(); // set app to run express function
 
 
+//View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //set path to views
+
+// body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set middleware for STATIC files (path)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/',function (req, res){
+  res.render('index',{
+      title: 'Audiophiles',
+    });
+});
+
 
 
 app.listen(8080, function(){
-  console.log('Server Stated on Port 8080...');
+  console.log('Server Started on Port 8080...');
 })
