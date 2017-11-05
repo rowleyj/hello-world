2a8220eac53c048c2b2f9d35913a681491096c00
Jesse Rowley <rowleyj@mcmaster.ca>
2017-11-04 23:09:07 -0400

    added partials
    header,footer, index.ejs

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+  //require statements
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongojs = require('mongojs'); //database scripting
 var path = require('path');
 
+  //set app to express
 var app = express(); // set app to run express function
 
-
-//View Engine
+  //View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); //set path to views
 
-// body-parser middleware
+  // body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//set middleware for STATIC files (path)
+  //set middleware for STATIC files (path)
 app.use(express.static(path.join(__dirname, 'public')))
 
+  //tell the browser to get the / directory which takes to index file
 app.get('/',function (req, res){
   res.render('index',{
       title: 'Audiophiles',
@@ -24,7 +26,7 @@ app.get('/',function (req, res){
 });
 
 
-
+  //set app.js to port 8080
 app.listen(8080, function(){
   console.log('Server Started on Port 8080...');
 })
