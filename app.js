100bb01921f7329e2bb2bded0d627a57a035053a
acadds <32973970+acadds@users.noreply.github.com>
2017-11-11 20:50:22 -0500

    adam user part 1

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
   //require statements
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongojs = require('mongojs'); //database scripting
-var path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongojs = require('mongojs'); //database scripting
+const mongoose = require('mongoose');
+const path = require('path');
+const expressValidator = require('express-validator')
+
+//connect to DB
+mongoose.connect('mongodb://localhost/audiophilesdb');
+  let db = mongoose.connection;
+
+//Check DB connection
+  db.once('open', function(){
+    console.log('Connected To MongoDB');
+  });
+
+
+//Check for DB Errors:
+  db.on('error',function(err){
+    console.log(err);
+  });
 
   //set app to express
-var app = express(); // set app to run express function
+const app = express(); // set app to run express function
 
-  //View Engine
+const router = express.Router();
+
+//View Engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); //set path to views
 
-  // body-parser middleware
+// body-parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-  //set middleware for STATIC files (path)
-app.use(express.static(path.join(__dirname, 'public')))
+//set middleware for STATIC files (path)
+app.use(express.static(path.join(__dirname, 'public')));
 
+//Validator middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root    = namespace.shift()
+    , formParam = root;
+
+  while(namespace.length){
+    formParam += '[' + namespace.shift() + ']';
+  }
+  return {
+    param: formParam,
+    msg: msg,
+    value: value
+  };
+  }
+}));
+
+//Bring In Models
+let Article = require('./public/js/mongofizz');
+
+//Add Route
+app.get('/views/signup', function(req, res){
+  res.render('/signup', {
+    title: 'Sign Up For Audiophiles'
+  });
+});
   //tell the browser to get the / directory which takes to index file
 app.get('/',function (req, res){
-  res.render('index',{
+  res.render('index', {
       title: 'Audiophiles',
-    });
+  });
 });
 //get browse ejs
 app.get('/browse',function (req, res){
@@ -44,13 +90,22 @@ app.get('/login',function (req, res){
 });
 //include get signup ejs
 app.get('/signup',function (req, res){
-  res.render('signup',{
-      title: 'Audiophiles',
+  Article.find({},function(err, articles){
+    if(err){
+      console.log(err);
+    } else {
+    res.render('signup', {
+      title: 'Sign Up For Audiophiles',
+      articles: articles
     });
+    }
+  });
 });
 
-
+//Add Router Routes
+let signup = require('./routes/signup');
+app.use('/signup', signup);
   //set app.js to port 8080
 app.listen(8080, function(){
   console.log('Server Started on Port 8080...');
-})
+});
